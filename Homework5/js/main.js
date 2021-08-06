17461474f58e07ed43bc0b0f848d08431e6d467e
VladyslavMartynov <vladyslav.martynov10@gmail.com>
2021-08-06 22:44:57 +0300

    Js first appereance!

---
+++ Homework5/js/main.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("menulist")
+const menuBtn = document.getElementById("menu__btn");
+
+menuBtn.addEventListener('click',()=>{
+    menu.classList.toggle('menu-active');
+});
+
+
+
+
+
